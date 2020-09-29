4c35377f3bab1f5be5bcd493ac527a75f5995ffd
sseungyong <sseungyong@gmail.com>
2020-09-30 00:07:07 +0900

    template inheritance and from

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
-from pybo import db
+from .. import db
+from ..forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
