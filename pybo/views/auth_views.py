2d37188eb0fac0d6928995f21bf77224b77b1cda
sseungyong <sseungyong@gmail.com>
2020-10-02 01:03:36 +0900

    modify models

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
+import functools
 
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
@@ -57,3 +58,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
